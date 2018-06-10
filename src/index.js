898f6058320b83607c4ea64b5ea02352056158c4
Stefan Ingolfsson <stefaningolfsson@Stefans-MacBook-Pro.local>
2018-06-10 16:03:56 +0100

    test

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import todoApp from "./reducers";
+import "normalize.css";
 import App from "./components/App";
 
 const store = createStore(todoApp);
